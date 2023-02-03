8714a5bf0bb21b8fd5cbe1269336a02004a08db9
Kevin Alexis Rondon Gonzalez <kevinalexisrondongonzalez@gmail.com>
2023-02-03 02:01:39 -0500

    completed project, in the future the syntax can be improved to make the code more readable

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const display = document.getElementById("display")
 const buttons = document.querySelectorAll("button")
-//Optimize the use of operator 
+//Optimize the use of operator in the future
 buttons.forEach((item)=>{
     item.onclick=()=>{
         if (item.id =="clear") {
@@ -8,11 +8,11 @@ buttons.forEach((item)=>{
         }else if (item.id == "backspace") {
             let string = display.innerText.toString()
             display.innerText = string.substring(0,string.length - 1)
-        }else if (display.innerText != "" && item.id == "aqual"){
+        }else if (display.innerText != "" && item.id == "equal"){
             //Optimize the use of eval by wrapping it inside a function to avoid security holes
             display.innerText = eval(display.innerText)
         }else if (display.innerText == "" && item.id == "equal"){
-            display.innerText("Null")
+            display.innerText = "Null"
             setTimeout(()=>(display.innerText = ""),2000)
         }else{
             display.innerText += item.id
@@ -20,4 +20,12 @@ buttons.forEach((item)=>{
         }
     } 
 })
-
+//Dark Mode
+const themeToggleBtn = document.querySelector(".theme-toggler")
+const calculator = document.querySelector(".calculator")
+let isDark = true
+themeToggleBtn.onclick = () => {
+    calculator.classList.toggle("dark")
+    themeToggleBtn.classList.toggle("active")
+    isDark = !isDark
+}
